d13b010aa6965410e288cf93c7e7b5f33e91eaff
Aarushi Rathore <165875037+AarushiRathore@users.noreply.github.com>
2025-04-06 17:37:40 +0530

    Update connect.php
    Signed-off-by: Aarushi Rathore <165875037+AarushiRathore@users.noreply.github.com>

--- connect.php
+++ connect.php
@@ -4,7 +4,6 @@ include 'db_connect.php'; // Include database connection
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $number = mysqli_real_escape_string($conn, $_POST['number']);
     $message = mysqli_real_escape_string($conn, $_POST['message']);
 
     $sql = "INSERT INTO contacts (name, email, subject, message) VALUES ('$name', '$email', '$number', '$message')";
