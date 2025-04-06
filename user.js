aaa099a0dba8aa6a8ab0e590d04f88111ec5a19d
Aarushi Rathore <165875037+AarushiRathore@users.noreply.github.com>
2025-04-06 19:52:23 +0530

    Update user.js
    Signed-off-by: Aarushi Rathore <165875037+AarushiRathore@users.noreply.github.com>

--- user.js
+++ user.js
@@ -23,4 +23,4 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 });
-module.exports = mongoose.model("User", UserSchema)s
+module.exports = mongoose.model("User", UserSchema)
