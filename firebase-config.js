ccea324be210f78b56c54b5dcc11e515a869bc39
Aarushi Rathore <165875037+AarushiRathore@users.noreply.github.com>
2025-04-19 13:36:27 +0530

    Update firebase-config.js
    Signed-off-by: Aarushi Rathore <165875037+AarushiRathore@users.noreply.github.com>

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,16 @@
-<script>
-  
+// firebase-config.js
+// Configuration for Firebase v9 compat (CDN version)
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-      apiKey: "AIzaSyBihkMtv01iKT1odnfH_6ilImu_-MijICo",
-      authDomain: "contactus-beff5.firebaseapp.com",
-      projectId: "contactus-beff5",
-      storageBucket: "contactus-beff5.firebasestorage.app",
-      messagingSenderId: "977845064641",
-      appId: "1:977845064641:web:c0e0a6103cd382ac4ad540",
-      measurementId: "G-LBE45KWC90"
-    };
-  
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-</script>
+  apiKey: "AIzaSyBihkMtv01iKT1odnfH_6ilImu_-MijICo",
+  authDomain: "contactus-beff5.firebaseapp.com",
+  projectId: "contactus-beff5",
+  storageBucket: "contactus-beff5.firebasestorage.app",
+  messagingSenderId: "977845064641",
+  appId: "1:977845064641:web:c0e0a6103cd382ac4ad540",
+  measurementId: "G-LBE45KWC90"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
